f6245f2283816f128484abb4a906c14b287e49aa
Pythebrian <64178799+Pythebrian@users.noreply.github.com>
2020-04-22 22:23:42 -0400

    Added program description

--- Neweggscraping.py
+++ Neweggscraping.py
@@ -9,7 +9,14 @@ headers = "Brand,Product name, Shipping, Price \n"
 f = open(out_filename, "w")
 f.write(headers)
 
+"""
+This short program will web scrape NewEgg. Currently it will only scrape one page. 
+It will return the brand, price, shipping and item name all at once. It will also
+export the collected information to an excel file
 
+This code was inspired by a tutorial on YouTube on how to use beautifulsoup!
+You can scrape different section of NewEgg by changing my_url below by pasting a new url in between the ''.
+"""
 
 my_url='https://www.newegg.ca/p/pl?N=100007708%2050001312%2050001314%2050001315%204814%20601201888%20601296707%20601331379%20600007787%20600358543%20600494828%20601303641%20600030348%20600100181'
 uClient = uReq(my_url)
